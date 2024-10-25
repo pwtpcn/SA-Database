d6bb92d0bceed44c3ae27094c45399951b6cb417
Thanapoom.C <palmdz08@gmail.com>
2024-10-26 02:07:39 +0700

    Create: create post put delete function for delivery_note

--- src/delivery_note.ts
+++ src/delivery_note.ts
@@ -1,15 +1,89 @@
 import { Elysia, t } from "elysia";
 import db from "./db";
 
-const app = new Elysia({prefix:"/delivery"});
+const app = new Elysia({ prefix: "/delivery" });
 
-app.get("/get", async () => {
+app.get(
+  "/get",
+  async () => {
     const deliveryList = await db.delivery_note.findMany();
     return deliveryList;
-},{
+  },
+  {
     detail: {
-        tags: ["DeliveryNote"]
-    }
-});
+      tags: ["DeliveryNote"],
+    },
+  }
+);
 
-export default app
+app.post(
+  "/post",
+  async ({ body }) => {
+    const note = await db.delivery_note.create({
+      data: body,
+    });
+    return note;
+  },
+  {
+    body: t.Object({
+      quotation_id: t.Number(),
+      sender_name: t.String(),
+      purchase_date: t.Optional(t.Date()),
+      reciever_sign: t.Optional(t.String()),
+      reciever_name: t.Optional(t.String()),
+    }),
+    detail: {
+      tags: ["DeliveryNote"],
+    },
+  }
+);
+
+app.put(
+  "/put",
+  async ({ body }) => {
+    const note = await db.delivery_note.update({
+      where: {
+        id: body.id,
+      },
+      data: body,
+    });
+    return note;
+  },
+  {
+    body: t.Object({
+      id: t.Number(),
+      quotation_id: t.Optional(t.Number()),
+      sender_name: t.Optional(t.String()),
+      purchase_date: t.Optional(t.Date()),
+      reciever_sign: t.Optional(t.String()),
+      reciever_name: t.Optional(t.String()),
+      receipt_id: t.Optional(t.Number()),
+      supplier_id: t.Optional(t.Number()),
+    }),
+    detail: {
+      tags: ["DeliveryNote"],
+    },
+  }
+);
+
+app.delete(
+    "/delete",
+    async ({ body }) => {
+      const note = await db.delivery_note.delete({
+        where: {
+          id: body.id,
+        },
+      });
+      return note;
+    },
+    {
+      body: t.Object({
+        id: t.Number(),
+      }),
+      detail: {
+        tags: ["DeliveryNote"],
+      },
+    }
+  );
+
+export default app;
